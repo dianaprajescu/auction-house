3b21b6e7111278c8289bcd1fed1a2f6264429cd6
diana <diana.prajescu@gmail.com>
2013-03-24 13:31:11 +0200

    Save user id and type on login.

--- src/GUI/GUIMediator.java
+++ src/GUI/GUIMediator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package GUI;
 
@@ -34,13 +34,16 @@ public class GUIMediator {
 	private SellerType seller;
 	private JList list;
 
+	/**
+	 * Login.
+	 */
 	public void login() {
 		// Connect db.
 		Database db = new Database();
-		
+
 		// Search if user exists in the DB.
 		ResultSet rs = db.query("SELECT * FROM user WHERE username = '" + username.getText() + "'");
-		
+
 		try {
 			if (!rs.next())
 			{
@@ -57,8 +60,13 @@ public class GUIMediator {
 							"Invalid Login", JOptionPane.ERROR_MESSAGE);
 					return;
 				}
+				else
+				{
+					// Set username id from db.
+					username.setId(rs.getInt("id"));
+				}
 			}
-	
+
 		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "Connection with DB lost!", "Connection lost", JOptionPane.ERROR_MESSAGE);
 			System.exit(0);
@@ -67,10 +75,12 @@ public class GUIMediator {
 		// Login user and save type.
 		if (buyer.isSelected())
 		{
+			username.setType(1);
 			rs = db.query("UPDATE user SET logged = '2', type = '1' WHERE username = '" + username.getText() + "'");
 		}
 		else if (seller.isSelected())
 		{
+			username.setType(2);
 			rs = db.query("UPDATE user SET logged = '2', type = '2' WHERE username = '" + username.getText() + "'");
 		}
 		else
@@ -79,58 +89,36 @@ public class GUIMediator {
 					"Invalid Login", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-		
+
 		// Close login frame.
 		login.setVisible(false);
-		
+
 		// Add username label in main frame.
 		JPanel mainPanel = (JPanel) gui.getContentPane().getComponent(0);
 		mainPanel.add(new JLabel("Welcome to Auction House, " + username.getText() + "!"), "North", 0);
-		
+
 		// Set gui frame visible.
 		gui.setVisible(true);
 	}
 
+	/**
+	 * Logout.
+	 */
 	public void logout() {
-		// Get current logged username.
-		String welcomeMessage = ((JLabel) ((JPanel) gui.getContentPane().getComponent(0)).getComponent(0)).getText();
-		String[] result = welcomeMessage.split("Welcome to Auction House, ");
-		String username = result[1].split("!")[0];
-		int type = 0;
-		int id = 0;
-		
-		// Get user type.
+		// Connect db.
 		Database db = new Database();
-		ResultSet rs = db.query("SELECT * FROM user WHERE username = '" + username + "'");
-		try
-		{
-			if (rs.next())
-			{
-				type = rs.getInt("type");
-				id = rs.getInt("id");
-			}
-			else
-			{
-				//  TODO remove all data related to this user.
-				JOptionPane.showMessageDialog(null, "Unexpected error!", "Logout problem", JOptionPane.ERROR_MESSAGE);
-				System.exit(0);
-			}
-		} catch (Exception e) {
-			//  TODO remove all data related to this user.
-			JOptionPane.showMessageDialog(null, "Unexpected error!" + e.getMessage(), "Logout problem", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		
+		ResultSet rs;
+
 		// Seller can logout only if he is not taking part into any auctions or if his offers are passed.
-		if (type == 2)
+		if (username.getType() == 2)
 		{
-			rs = db.query("SELECT * FROM offer WHERE seller_id = '" + id + "'");
+			rs = db.query("SELECT * FROM offer WHERE seller_id = '" + username.getId() + "'");
 			try {
 				// The seller is taking part in some auctions.
 				if (rs.next())
 				{
 					// TODO Are the seller's offers passed?
-					
+
 				}
 
 			} catch (SQLException e) {
@@ -139,164 +127,143 @@ public class GUIMediator {
 				System.exit(0);
 			}
 		}
-		
+
 		// When buyer loogs out he leaves all active auctions, declining all offers.
 		// If there is no offer, his request is canceled.
-		
+
 		// Logging out during service transfer will cause the transfer to fail and it'll be marked accordingly.
-		
+
 		// Logout.
-		rs = db.query("UPDATE user SET logged = '1' WHERE username = '" + username + "'");
+		rs = db.query("UPDATE user SET logged = '1' WHERE username = '" + username.getText() + "'");
 		gui.dispose();
-		
+
 		JOptionPane.showMessageDialog(null, "Logged out successfully!", "Good bye!", JOptionPane.INFORMATION_MESSAGE);
 		login.setVisible(true);
 	}
-	
+
+	/**
+	 * Show service option list.
+	 *
+	 * @param   Table  table  Source of the mouse event.
+	 * @param   int    x      The x coordinate where the right click was made.
+	 * @param   int    y      The y coordinate where the right click was made.
+	 */
 	public void showServicePopup(Table table, int x, int y)
-	{				
+	{
+		// Get service status.
 		String status = (String) this.gui.getTable().getModel().getValueAt(this.gui.getTable().getSelectedRow(), 2);
-		System.out.println(status);
 		int intStatus = this.getIntStatus(status);
-		
-		JPopupMenu popup = new JPopupMenu();
-		
+
 		//TODO ia din baza de date optiunile disponibile pentru serviciul selectat.
-		
-		// Get user type from db.
-		int type = 0;
-		Database db = new Database();
-		ResultSet rs = db.query("SELECT * FROM user WHERE username = '" + username.getText() + "'");
-		try
-		{
-			rs.next();
-			type = rs.getInt("type");
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Unexpected error!" + e.getMessage(), "Problem", JOptionPane.ERROR_MESSAGE);
-		}
-		
+
 		// If user is a buyer.
-		if (type == 1)
+		if (username.getType() == 1)
 		{
+			// Create popup menu for services.
+			JPopupMenu popup = new JPopupMenu();
+
 			if (intStatus == 1)
 			{
 				popup.add(new PopupMenuItem("Launch Offer Request", this, this.gui));
 			}
 			else if (intStatus < 5)
 			{
+				// TODO only if no offer made.
 				popup.add(new PopupMenuItem("Drop Offer Request", this, this.gui));
 			}
+
+			// Show the menu.
+			popup.show(table, x, y);
 		}
-		else
-		{
-	        popup.add(new PopupMenuItem("option1", this, this.gui));
-	        popup.add(new PopupMenuItem("option2", this, this.gui));
-		}
-        
-        popup.show(table, x, y); //and show the menu
 	}
-	
+
 	public void showListPopup(JList list, int x, int y)
 	{
 		String user = (String) list.getSelectedValue();
-		
+
 		String status = (String) this.gui.getTable().getModel().getValueAt(this.gui.getTable().getSelectedRow(), 2);
 		int intStatus = this.getIntStatus(status);
-		
-		JPopupMenu popup = new JPopupMenu();
-		
-		//TODO ia din baza de date optiunile disponibile pentru user selectat.
-		
-		// Get user type from db.
-		int type = 0;
-		Database db = new Database();
-		ResultSet rs = db.query("SELECT * FROM user WHERE username = '" + username.getText() + "'");
 
-		try
-		{
-			rs.next();
-			type = rs.getInt("type");
-			
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Unexpected error!" + e.getMessage(), "Problem", JOptionPane.ERROR_MESSAGE);
-		}
-		
+		JPopupMenu popup = new JPopupMenu();
+
+		//TODO ia din baza de date optiunile disponibile pentru user selectat.
+
 		// If user is a buyer.
-		if (type == 1)
+		if (username.getType() == 1)
 		{
 			popup.add(new PopupMenuItem("Accept Offer", this, this.gui));
-			
+
 	        popup.add(new PopupMenuItem("Refuse Offer", this, this.gui));
 		}
 		else
 		{
 			popup.add(new PopupMenuItem("Make Offer", this, this.gui));
-			
+
 			if (intStatus == 4)
 			{
 				popup.add(new PopupMenuItem("Drop auction", this, this.gui));
 			}
 		}
-        
+
         popup.show(list, x, y); //and show the menu
 	}
-	
+
 	public void userAction(String command)
 	{
 		//TODO do sth based on command.
 		System.out.println(command);
 		System.out.println(this.gui.getTable().getModel().getValueAt(this.gui.getTable().getSelectedRow(), 0));
 	}
-	
+
 	/**
 	 * Method used to transform the status into it's int equivalent.
-	 * 
+	 *
 	 * @param   String  status  The string status.
-	 * 
+	 *
 	 * @return  int  The int status equivalent.
 	 */
 	private int getIntStatus(String status)
 	{
 		// Status int db values.
 		HashMap hm = new HashMap(10);
-		hm.put((Object) "inactive", (Object) new Integer(1));
-		hm.put((Object) "no offer", (Object) new Integer(2));
-		hm.put((Object) "offer made", (Object) new Integer(3));
-		hm.put((Object) "offer exceeded", (Object) new Integer(4));
-		hm.put((Object) "offer accepted", (Object) new Integer(5));
-		hm.put((Object) "offer refused", (Object) new Integer(6));
-		hm.put((Object) "transfer started", (Object) new Integer(7));
-		hm.put((Object) "transfer in progress", (Object) new Integer(8));
-		hm.put((Object) "transfer completed", (Object) new Integer(9));
-		hm.put((Object) "transfer failed", (Object) new Integer(10));
-		
+		hm.put("inactive", new Integer(1));
+		hm.put("no offer", new Integer(2));
+		hm.put("offer made", new Integer(3));
+		hm.put("offer exceeded", new Integer(4));
+		hm.put("offer accepted", new Integer(5));
+		hm.put("offer refused", new Integer(6));
+		hm.put("transfer started", new Integer(7));
+		hm.put("transfer in progress", new Integer(8));
+		hm.put("transfer completed", new Integer(9));
+		hm.put("transfer failed", new Integer(10));
+
 		return ((Integer) hm.get(status.toLowerCase())).intValue();
 	}
-	
+
 	public void registerGUI(GUI gui) {
 		this.gui = gui;
 	}
-	
+
 	public void registerLogin(Login login) {
 		this.login = login;
 	}
-	
+
 	public void registerUsername(UsernameField username) {
 		this.username = username;
 	}
-	
+
 	public void registerPassword(PasswordField password) {
 		this.password = password;
 	}
-	
+
 	public void registerBuyerType(BuyerType buyer) {
 		this.buyer = buyer;
 	}
-	
+
 	public void registerSellerType(SellerType seller) {
 		this.seller = seller;
 	}
-	
+
 	public void registerList(JList list){
 		this.list = list;
 	}
--- src/GUI/components/UsernameField.java
+++ src/GUI/components/UsernameField.java
@@ -3,8 +3,6 @@
  */
 package GUI.components;
 
-import interfaces.IGUIMediator;
-
 import javax.swing.JTextField;
 
 import GUI.GUIMediator;
@@ -15,9 +13,59 @@ import GUI.GUIMediator;
  */
 public class UsernameField extends JTextField {
 	private GUIMediator med;
+	
+	/**
+	 * User id from db.
+	 */
+	private int id;
+	
+	/**
+	 * User type (buyer = 1, seller = 2).
+	 */
+	private int type;
 
 	public UsernameField(GUIMediator med) {
 		this.med = med;
 		med.registerUsername(this);
+	}
+	
+	/**
+	 * Setter for id.
+	 * 
+	 * @param   int  id  The id to set.
+	 */
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	/**
+	 * Getter for id.
+	 * 
+	 * @return  int  The id.
+	 */
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	/**
+	 * Setter for type.
+	 * 
+	 * @param   int  type  The type to set.
+	 */
+	public void setType(int type)
+	{
+		this.type = type;
+	}
+	
+	/**
+	 * Getter for type.
+	 * 
+	 * @return  int  The type.
+	 */
+	public int getType()
+	{
+		return this.type;
 	}
 }
